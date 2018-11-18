07b88c273ecc807d60609d13e92130698652fb65
sijiawang <sijiawang@wustl.edu>
2018-11-18 15:55:17 -0600

    mix std normalization

--- SRC.m
+++ SRC.m
@@ -13,65 +13,66 @@ function [label, iterationCount] = SRC(tr, te, epsilon, lambda, options)
 %       [2] https://github.com/hiroyuki-kasai/ClassifierToolbox
 %       [3] L. Zhang, et all. A Simple Homotopy Algorithm for Compressive Sensing
 
-% get the size of datesets
-[~, n] = size(tr.x);
-[~, k] = size(te.x);
+    % get the size of datesets
+    [~, n] = size(tr.x);
 
-% extract options    
-if ~isfield(options, 'eigenface')
-    eigenface = true;
-else
-    eigenface = options.eigenface;
-end    
+    % extract options    
+    if ~isfield(options, 'eigenface')
+        eigenface = true;
+    else
+        eigenface = options.eigenface;
+    end    
 
-if ~isfield(options, 'eigenface_dim')
-    eigenface_dim = n;
-else
-    eigenface_dim = options.eigenface_dim;
-end
-
-% generate eigenface
-if eigenface    
-    [disc_set, ~, ~] = Eigenface_f(tr.x, eigenface_dim);
-    tr.x  =  disc_set' * tr.x;
-    te.x  =  disc_set' * te.x;
-end
-
-% normalize the columns of A to have unit l2-norm
-tr.x = tr.x ./ sqrt(sum(tr.x .^ 2, 2));
-te.x = te.x ./ sqrt(sum(te.x .^ 2, 2));
-
-% get the class label
-classes = unique(tr.y);
-class_num = length(classes);
-
-% solve the l1-minimization problem
-P = tr.x'/(tr.x'*tr.x+1e-5*eye(n));
-label = zeros(1,k);
-iterationCount = zeors(1,k);
-for i = 1:k
-    % tex is a sample to predict
-    tex = te.x(:,i);
-    x0 = P*tex;
-
-    % implement Homotopy algorithm
-    [xp, iterationCount(i)] = SolveHomotopy(tr.x, tex, ...
-                            'maxIteration', 5000,...
-                            'isNonnegative', false, ...
-                            'stoppingCriterion', -1, ...
-                            'groundtruth', x0, ...
-                            'lambda', lambda, ...
-                            'tolerance', epsilon);  
-
-    % calculate residual for each class
-    residuals = zeros(1, max(classes));
-    for j = 1 :class_num
-        idx = find(tr.y == classes(j));
-        residuals(round(classes(j))) = norm(te.x - tr.x(:,idx)*xp(idx));
+    if ~isfield(options, 'eigenface_dim')
+        eigenface_dim = n;
+    else
+        eigenface_dim = options.eigenface_dim;
     end
 
-    % get the predicted label with minimum residual
-    [~, label(i)] = min(residuals); 
+    % generate eigenface
+    if eigenface    
+        [disc_set, ~, ~] = Eigenface_f(tr.x, eigenface_dim);
+        tr.x  =  disc_set' * tr.x;
+        te.x  =  disc_set' * te.x;
+    end
+    [~, n] = size(tr.x);
+    [~, k] = size(te.x);
+    % normalize the columns of A to have unit l2-norm
+    tr.x = tr.x./repmat(sqrt(sum(tr.x.^2)),[size(tr.x,1),1]);
+    te.x = te.x./repmat(sqrt(sum(te.x.^2)),[size(te.x,1),1]);
+    
+    % get the class label
+    classes = unique(tr.y);
+    class_num = length(classes);
+
+    % solve the l1-minimization problem
+    P = inv(tr.x'*tr.x+1e-5*eye(n))*tr.x';
+    label = zeros(1,k);
+    iterationCount = zeros(1,k);
+    for i = 1:k
+        % tex is a sample to predict
+        tex = te.x(:,i);
+        x0 = P*tex;
+
+        % implement Homotopy algorithm
+        [xp, iterationCount(i)] = SolveHomotopy(tr.x, tex, ...
+                                'maxIteration', 5000,...
+                                'isNonnegative', false, ...
+                                'stoppingCriterion', -1, ...
+                                'groundtruth', x0, ...
+                                'lambda', lambda, ...
+                                'tolerance', epsilon);  
+
+        % calculate residual for each class
+        residuals = zeros(1, max(classes));
+        for j = 1 :class_num
+            idx = find(tr.y == classes(j));
+            residuals(round(classes(j))) = norm(te.x - tr.x(:,idx)*xp(idx));
+        end
+
+        % get the predicted label with minimum residual
+        [~, label(i)] = min(residuals); 
+    end
 end
 
 
