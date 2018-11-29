ca2c27e40c7e903ea978a5098e331f274a0943e4
sijiawang <sijiawang@wustl.edu>
2018-11-28 18:56:46 -0600

    path config

---
+++ run_me_first.m
@@ -0,0 +1,22 @@
+% Add folders to path.
+
+addpath(pwd);
+
+cd datasets/;
+addpath(genpath(pwd));
+cd ..;
+
+cd algorithms/;
+addpath(genpath(pwd));
+cd ..;
+
+cd utils;
+addpath(genpath(pwd));
+cd ../;
+
+cd demo;
+addpath(genpath(pwd));
+cd ../;
+
+
+
