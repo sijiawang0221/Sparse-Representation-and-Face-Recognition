f1eede66266c0873c994371a865d5ea0d86b7d8e
sijiawang <sijiawang@wustl.edu>
2018-11-30 20:40:10 -0600

    demo

--- demo/demo_SRC_downsample.m
+++ demo/demo_SRC_downsample.m
@@ -7,35 +7,32 @@ cd ..;
 fprintf('running experiment\n\t SRC with eigenfaces on CMU database...\n')
 load('CMU_Dataset_new.mat')
 % set up dataset structure
-TrainSet = {};
+TrainSet   = {};
 TrainSet.X = CMUTrain;
 TrainSet.y = classTrain;
-TestSet = {};
-TestSet.X = CMUTest;
-TestSet.y = classTest;
+TestSet    = {};
+TestSet.X  = CMUTest;
+TestSet.y  = classTest;
 % given downsample factors
 s = [9 10 12 18 20 24 30 40 60];
 % experiment results: error and runtime
 [err_CMU_down, t_CMU_down] = run_SRC_downsample(TrainSet, TestSet, [1],120,128);
 
 
-
-
 %% SRC with eigenfaces on Yale database
 fprintf('running experiment\n\t SRC with eigenfaces on Yale database...\n')
 load('Yale_face_full.mat'); 
 % set up dataset structure
-TrainSet = {};
+TrainSet   = {};
 TrainSet.X = YaleTrain;
 TrainSet.y = classTrain;
-TestSet = {};
-TestSet.X = YaleTest;
-TestSet.y = classTest;
+TestSet    = {};
+TestSet.X  = YaleTest;
+TestSet.y  = classTest;
 % given downsample factors
 s = [6 7 13 15 18 25 28 33 40 57 80];
 % experiment results: error and runtime
 [err_Yale_eigen, t_Yale_eigen] = run_SRC_downsample(TrainSet, TestSet, s, 192,168);
-
 
 
 %% experiment on AR database
